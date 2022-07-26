317c3188d299e7cf1e561c6198e961ebe9863069
Neville Yee <neville.yee@alumni.york.ac.uk>
2022-07-26 09:24:22 +0000

    Added image IO module.

---
+++ src/Quagga/io.py
@@ -0,0 +1,87 @@
+# Copyright 2021 Rosalind Franklin Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+
+import numpy as np
+from skimage import io
+
+import matplotlib.pyplot as plt
+from mpl_point_clicker import clicker
+
+
+def open_image(path,
+               pw_nm,
+               sample_dims_um,
+               sample_points,
+               sample_offset_um,
+               idx=0,
+               trunc=1,
+               num_ramps=1,
+               normalise=True):
+    """
+    Method to access image and let user pick patches for assessment
+
+    Args:
+    path (str) :: Path to raw image
+    pw_nm (float) :: Pixel width in um
+    sample_dims_um (2-tuple) :: Dimensions of patches in um
+    sample_points (2-tuple) :: Number of patches in each direction for each ramp
+    sample_offset_um (float) :: Separation of patches in um
+    idx (int) :: Index of image to be assessed (if image is in a stack)
+    trunc (int) :: Number of pixels to be truncated from image bottom (due to information bar)
+    num_ramps (int) :: Number of ramps in image
+    normalise (bool) :: Whether to normalise image
+
+    Returns:
+    ndarray*4
+    """
+    img = io.imread(path)
+    if img.ndim == 2:
+        img = img[np.newaxis, :, :-trunc]
+
+    sample_dims_px = np.array(sample_dims_um)*1000 // np.array(pw_nm)
+    sample_dims_half = sample_dims_px // 2
+
+    # Collect user clicks
+    fig, ax = plt.subplots(figsize=(15, 9))
+    plt.title(f"{path}: {num_ramps} ramps")
+    ax.imshow(img[idx, :-trunc], cmap="gist_gray")
+
+    clicks = np.array(plt.ginput(num_ramps), dtype=int)
+    clicks = clicks[:, ::-1]
+
+    # Convert sample offset to pixels and create mesh
+    sample_offset_px = np.array(sample_offset_um)*1000 // np.array(pw_nm)
+    offsets_x_px = np.array(np.arange(-0.5*(sample_points[0]-1),
+                                      0.5*(sample_points[0]+1)*sample_offset_px,
+                                      dtype=int))
+    offsets_y_px = np.array(np.arange(-0.5*(sample_points[1]-1),
+                                      0.5*(sample_points[1]+1)*sample_offset_px,
+                                      dtype=int))
+
+    cntrs, uls, lrs = [], [], []
+    for _, click in enumerate(clicks):
+        for _, osx in enumerate(offsets_x_px):
+            for _, osy in enumerate(offsets_y_px):
+                cntrs.append(click + np.array([osx, osy]))
+                uls.append(click + np.array([osx, osy]) - sample_dims_half)
+                lrs.append(click + np.array([osx, osy]) + sample_dims_half)
+
+    # Normalisation of image
+    midrange = [np.percentile(img, 10), np.percentile(img, 90)]
+    img_cec = (img-midrange[0]) / (midrange[1]-midrange[0])
+    img_cec[img_cec<0] = 0
+    img_cec[img_cec>1] = 1
+
+    return img_cec, np.array(cntrs, dtype=int), np.array(uls, dtype=int), np.array(lrs, dtype=int)
